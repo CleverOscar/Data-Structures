d06cc1fc8e7a632ad73520d2f600bd23362ddbab
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-07-18 14:36:55 -0500

    Single Node Class
    Starting of with single node since linking serval nodes give us a complete list.

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,10 @@
+class Node:
+    # Constructor
+    def __init__(self, data,next=None):
+        self.data = data
+        self.next = next
+
+
+#Uncomment to create a single node
+# first = Node(3)
+# print(first.data)
