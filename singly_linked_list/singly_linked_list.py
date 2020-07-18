be6e8860da726b1c4446697c79e4e77cd3fc24c6
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-07-18 15:08:25 -0500

    Few adjustments
    added simple node checker method to check if a node exists or not

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -4,15 +4,23 @@ class Node:
         self.data = data
         self.next = next
 
+    def has_value(self, data):
+        if self.data == data:
+            return True
+        else:
+            return False
+
 
 #Uncomment to create a single node
-# first = Node(3)
-# print(first.data)
+first = Node(3)
+print(first.data)
+print(first.has_value(3))
 
 
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def insert(self, data):
         newNode = Node(data)
@@ -25,8 +33,20 @@ class LinkedList:
         else:
             self.head = newNode
 
+    def add_to_head(self, data):
 
-#Uncomment to see Linked List with Single Node
-# LL = LinkedList()
-# LL.head = Node(3)
-# print(LL.head.data)
+        if self.head > 0:
+            self.head = data[0]
+        else:
+            self.head.data = data
+
+
+
+
+
+'''
+    Uncomment to see Linked List with Single Node
+    LL = LinkedList()
+    LL.head = Node(3)
+ print(LL.head.data)
+'''
