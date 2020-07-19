cb37eae6f48cf0869bbe5ad2700b6469d4edad21
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-07-19 00:13:49 -0500

    LinkedList Class Broken
    Linked List node get_Max() needs to be fixed

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,52 +1,73 @@
-class Node:
+class ListNode:
     # Constructor
-    def __init__(self, data,next=None):
-        self.data = data
-        self.next = next
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-    def has_value(self, data):
-        if self.data == data:
-            return True
-        else:
-            return False
+    def get_value(self):
+        return self.value
 
+    def get_next(self):
+        return self.next
 
-#Uncomment to create a single node
-first = Node(3)
-print(first.data)
-print(first.has_value(3))
+    def set_next(self, new_next):
+        self.next = new_next
 
 
-class LinkedList:
+
+class LinkedList():
     def __init__(self):
         self.head = None
         self.tail = None
 
-    def insert(self, data):
-        newNode = Node(data)
+    def add_to_tail(self, value):
+        new_node = ListNode(value)
 
-        if(self.head):
-            current = self.head
-            while(current.next):
-                current = current.next
-            current.next = newNode
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
         else:
-            self.head = newNode
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
-    def add_to_head(self, data):
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return
+        # what is we only have a single elem in the LinkedList?
+        # both head and tail are pointing at the same Node
+        if not self.head.get_next():
+            self.head = self.head
+            # delete the linked list head reference
+            self.head = None
+            # also need to delete tail reference
+            self.tail = None
+            return self.head.get_value()
 
-        if self.head > 0:
-            self.head = data[0]
-        else:
-            self.head.data = data
+        val = self.head.get_value()
+        # set self.head to the Node after the head
+        self.head = self.head.get_next()
+        return val
 
+    def remove_tail(self):
+        if self.head is None and self.tail is None:
+            return None
 
+        current = self.head
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+        # at this point current is the node
 
+        self.tail = None
 
+        self.tail = current
 
-'''
-    Uncomment to see Linked List with Single Node
-    LL = LinkedList()
-    LL.head = Node(3)
- print(LL.head.data)
-'''
+    def get_max(self):
+        
+
+# linkedlist = ListNode(1)
+# print(linkedlist.data)
+# linkedlist.set_next(ListNode(2))
+# print(linkedlist.next.data)
+# linkedlist.next.set_next(ListNode(3))
+# print(linkedlist.next.next.data)
+# print(linkedlist.next.next.next)
